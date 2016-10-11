b253f86d2256637c9aab88fe01290edcbaef5f80
mtomanski <mtomanski@nexelem.com>
2016-10-11 21:51:59 +0200

    one

--- app/pl/mtomanski/fsmdemo/machine/TicketMachine.scala
+++ app/pl/mtomanski/fsmdemo/machine/TicketMachine.scala
@@ -12,7 +12,46 @@ import scala.concurrent.duration._
 import scala.reflect._
 
 class TicketMachine(gatewayActor: ActorRef, connectionActor: ActorRef,
-                    reservationActor: ActorRef, printOutActor: ActorRef)
+                    reservationActor: ActorRef, printOutActor: ActorRef) extends PersistentFSM[TicketMachineState, TicketMachineContext, TicketMachineEvent] {
+
+
+  override def domainEventClassTag: ClassTag[TicketMachineEvent] = classTag[TicketMachineEvent]
+
+  override def persistenceId: String = "TicketMachine"
+
+  override def applyEvent(domainEvent: TicketMachineEvent, currentData: TicketMachineContext): TicketMachineContext =
+    domainEvent match {
+      case TicketMachineCreated(id, origin) =>
+        ContextWithOrigin(id, origin)
+    }
+
+  startWith(Idle, Empty)
+
+  when(Idle) {
+    case Event(CreateTicketMachine(origin), Empty) =>
+      val id = TicketMachineIdGenerator.generate
+      goto(FetchingSoonestConnections) applying TicketMachineCreated(id, origin) replying id
+  }
+
+  onTransition {
+    case Idle -> FetchingSoonestConnections =>
+      nextStateData match {
+        case ContextWithOrigin(id, origin) => {
+          println("Going to FetchingSoonestConnections")
+          connectionActor ! FetchSoonestConnections(origin)
+        }
+      }
+  }
+
+  override def onRecoveryCompleted(): Unit = println("Recovery completed :-)")
+
+  override def onRecoveryFailure(cause: Throwable, event: Option[Any]): Unit = {
+    println("Recovery failed")
+    super.onRecoveryFailure(cause, event)
+  }
+
+}
+
 
 
 
